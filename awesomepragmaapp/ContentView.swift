9abc30e91b985dbba67a69e2ec1f6c003edf4782
Dario Carlomagno <dario.carlomagno@gmail.com>
2019-10-05 00:19:21 +0100

    setup project

---
+++ awesomepragmaapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  awesomepragmaapp
+//
+//  Created by Dario Carlomagno on 05/10/2019.
+//  Copyright © 2019 Bold Ideas ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World 👋")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
